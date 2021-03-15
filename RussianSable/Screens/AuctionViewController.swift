1d4a301a1c7026530c0d47d8678fdfb98db31376
Kirill Kornakov <sniper_kir@mail.ru>
2021-03-16 01:31:55 +0800

    Added view controllers

--- RussianSable/Screens/AuctionViewController.swift
+++ RussianSable/Screens/AuctionViewController.swift
@@ -19,6 +19,21 @@ class AuctionViewController: UIViewController {
       
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AuctionCell", for: indexPath)
+        
+        cell.auctionNumberLabel?.text = "test"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
 
  
 
